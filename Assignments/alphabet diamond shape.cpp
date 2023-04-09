d9de710b43736af378c352ad87e390cc51062515
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-04-09 18:01:41 +0300

    Update alphabet diamond shape.cpp

--- Assignments/alphabet diamond shape.cpp
+++ Assignments/alphabet diamond shape.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include<cmath>
 
+using namespace std;
+int main() {
+    for(int i = 0; i < 15; i++)
+    {
+        for(int j = 0; j < 15; j++)
+        {
+            int column =  7 - abs(7 - j);
+            int row =  7 - abs(7 - i);
+            if (column + row <= 7)
+            {
+                cout<<char(column+ 65)<<" ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}  
